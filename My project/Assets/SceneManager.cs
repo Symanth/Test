13610c05c35f49099f574f4c62b113b9a2a34190
DESKTOP-4N6B22F\HRM <frizen12@gmail.com>
2022-10-01 15:01:08 +0300

    commint #Method

--- My project/Assets/SceneManager.cs
+++ My project/Assets/SceneManager.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializedField] private bool state;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start scene");
+        Method(state);
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (true) 
-       {
-            Debug.Log("Start");
-       }
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    private void Method(bool state)
+    {
+        if (state)
+        {
+            Debug.Log("State: True");
+        }
     }
 }
